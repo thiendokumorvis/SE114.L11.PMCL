99e3edd976fac355dabdcee2f21146a8d6295eb9
vytran0710 <56116908+vytran0710@users.noreply.github.com>
2020-11-02 17:35:01 +0700

    Update

--- GireShoppingApp/app/src/main/java/edu/uit/gireshoppingapp/LoginActivity.java
+++ GireShoppingApp/app/src/main/java/edu/uit/gireshoppingapp/LoginActivity.java
@@ -17,12 +17,15 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
     private FirebaseAuth mAuth;
+
     private EditText loginEmail;
     private EditText loginPassword;
     private Button loginButton;
-    private Button registerButton;
+    private Button goToRegisterButton;
     private ProgressBar loginProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,35 +34,39 @@ public class LoginActivity extends AppCompatActivity {
         loginEmail = findViewById(R.id.loginEmail);
         loginPassword = findViewById(R.id.loginPassword);
         loginButton = findViewById(R.id.loginButton);
-        registerButton = findViewById(R.id.goToRegisterButton);
+        goToRegisterButton = findViewById(R.id.goToRegisterButton);
         loginProgressBar = findViewById(R.id.loginProgressBar);
-
         mAuth = FirebaseAuth.getInstance();
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                loginProgressBar.setVisibility(View.VISIBLE);
+
                 String email = loginEmail.getText().toString();
                 String password = loginPassword.getText().toString();
                 if(email.length() <= 8 || password.length() <= 8)
+                {
                     Toast.makeText(LoginActivity.this, "Email and password must be more than 8 characters.",
                             Toast.LENGTH_SHORT).show();
+                    loginProgressBar.setVisibility(View.GONE);
+                }
                 else
                     loginButtonOnPress(email, password);
             }
         });
     }
+
     private void loginButtonOnPress(String email, String password)
     {
-        loginProgressBar.setVisibility(View.VISIBLE);
         mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
                             Toast.makeText(LoginActivity.this, "Authentication succeeded.",
                                     Toast.LENGTH_SHORT).show();
-                            FirebaseUser user = mAuth.getCurrentUser();
                             loginProgressBar.setVisibility(View.GONE);
                         } else {
                             Toast.makeText(LoginActivity.this, "Authentication failed.",
