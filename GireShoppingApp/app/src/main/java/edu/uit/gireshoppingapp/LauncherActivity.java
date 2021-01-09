b11fe4098f48f6256b8b9b161a7f1ccb211bb8d5
vytran0710 <56116908+vytran0710@users.noreply.github.com>
2021-01-09 18:18:50 +0700

    Keep user signed in

--- GireShoppingApp/app/src/main/java/edu/uit/gireshoppingapp/LauncherActivity.java
+++ GireShoppingApp/app/src/main/java/edu/uit/gireshoppingapp/LauncherActivity.java
@@ -8,15 +8,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LauncherActivity extends AppCompatActivity {
 
     private Button login_button;
     private TextView register_button;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        if(mAuth.getInstance().getCurrentUser() != null)
+        {
+            Intent intent = new Intent(LauncherActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
         login_button = findViewById(R.id.login_button);
         register_button = findViewById(R.id.register_button);
